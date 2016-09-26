cd486342da14c17195e74d76ce0f4603d8605d74
Igor Descayrac <igor.descayrac@gmail.com>
2016-09-26 09:33:54 +0200

    Extract method for cell neighbours

--- minefield/hinter.rb
+++ minefield/hinter.rb
@@ -1,24 +1,38 @@
 class MineFieldHinter
 
+  # Returns number of adjacent mines
   def self.hint(minefield, x, y)
-    hint = 0
-
-    edges = edges(minefield, x, y)
-
-    hint += 1 if !edges.include?(:top) && minefield.field[(x - 1)][y]
-    hint += 1 if !edges.include?(:left) && minefield.field[x][(y - 1)]
-    hint += 1 if !edges.include?(:bottom) && minefield.field[(x + 1)][y]
-    hint += 1 if !edges.include?(:right) && minefield.field[x][(y + 1)]
-
-    hint += 1 if (edges & [:top, :left]).empty? && minefield.field[(x - 1)][(y - 1)]
-    hint += 1 if (edges & [:top, :right]).empty? && minefield.field[(x - 1)][(y + 1)]
-    hint += 1 if (edges & [:bottom, :left]).empty? && minefield.field[(x + 1)][(y - 1)]
-    hint += 1 if (edges & [:bottom, :right]).empty? && minefield.field[(x + 1)][(y + 1)]
-
-    hint
+    neighbours(minefield, x, y).count do |neighbour_x, neighbour_y|
+      minefield.mine_at?(neighbour_x, neighbour_y)
+    end
   end
 
-  # Returns array with adjactent edges : :top, :left, :bottom, :right  
+  # Returns array with coords of adjacent cells
+  def self.neighbours(minefield, x, y)
+    edges = edges(minefield, x, y)
+
+    orthogonal_neighbours(x, y, edges) + diagonal_neighbours(x, y, edges)
+  end
+
+  def self.orthogonal_neighbours(x, y, edges)
+    neighbours = []
+    neighbours << [(x - 1), y] unless edges.include?(:top)
+    neighbours << [x, (y - 1)] unless edges.include?(:left)
+    neighbours << [(x + 1), y] unless edges.include?(:bottom)
+    neighbours << [x, (y + 1)] unless edges.include?(:right)
+    neighbours
+  end
+
+  def self.diagonal_neighbours(x, y, edges)
+    neighbours = []
+    neighbours << [(x - 1), (y - 1)] if (edges & [:top, :left]).empty?
+    neighbours << [(x - 1), (y + 1)] if (edges & [:top, :right]).empty?
+    neighbours << [(x + 1), (y - 1)] if (edges & [:bottom, :left]).empty?
+    neighbours << [(x + 1), (y + 1)] if (edges & [:bottom, :right]).empty?
+    neighbours
+  end
+
+  # Returns array with adjacent edges : :top, :left, :bottom, :right
   def self.edges(minefield, x, y)
     edges = []
     edges << :top    if x.zero?
